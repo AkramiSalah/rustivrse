050d4fb0df13b9f1a3ee08621c9fb77a14eb8080
Silveronski <153466946+Silveronski@users.noreply.github.com>
2023-12-11 18:30:43 +0200

    refactoring entire code
    changed function,variables names.
    refactored workflow of map.js
    removed unnecessary elements and styles.
    asking your permission to push the great leader akrami.

--- map.js
+++ map.js
@@ -1,45 +1,46 @@
 const map = document.querySelector(".map");
 let isDragging = false;
+
 let startPointX = 0;
 let startPointY = 0;
-let distanceTraveledX = 0;
-let distanceTraveledY = 0;
 
-let shiftedX = 0;
-let shiftedY = 0;
+let currentDragX = 0;
+let currentDragY = 0;
+
+let totalDragX = 0;
+let totalDragY = 0;
 
 let mapWidth = 0;
 let mapHeight = 0;
 
-// Event listeners for drag start on mouse and touch
-map.addEventListener('mousedown', handleStart);
-map.addEventListener('touchstart', handleStart);
+// Event listeners for drag start - mouse and touch
+map.addEventListener('mousedown', handleDragStart);
+map.addEventListener('touchstart', handleDragStart);
 
-function handleStart(e) {
+function handleDragStart(e) {
     isDragging = true;
     if (e.type === 'mousedown') {
-        // Set starting point for mouse drag
         startPointX = e.clientX;
         startPointY = e.clientY;
     } else if (e.type === 'touchstart') {
-        // Set starting point for touch drag
         startPointX = e.touches[0].clientX;
         startPointY = e.touches[0].clientY;
     }
     map.style.cursor = "grab";
 }
 
-// Event listeners for drag move on mouse and touch
-document.addEventListener('mousemove', handleMove);
-document.addEventListener('touchmove', handleMove);
+// Event listeners for drag move - mouse and touch
+document.addEventListener('mousemove', handleDragMove);
+document.addEventListener('touchmove', handleDragMove);
 
-function handleMove(e) {
+
+function handleDragMove(e) {
     if (isDragging) {
         const clientX = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
         const clientY = e.type === 'mousemove' ? e.clientY : e.touches[0].clientY;
 
-        const deltaX = clientX - startPointX - distanceTraveledX;
-        const deltaY = clientY - startPointY - distanceTraveledY;
+        const deltaX = clientX - startPointX - currentDragX;
+        const deltaY = clientY - startPointY - currentDragY;
 
         const currentX = parseFloat(getComputedStyle(map).backgroundPositionX);
         const currentY = parseFloat(getComputedStyle(map).backgroundPositionY);
@@ -50,19 +51,68 @@ function handleMove(e) {
         // Update background position within specified bounds
         if (newX <= 500 && newX >= -500) {
             map.style.backgroundPositionX = `${newX}px`;
-            distanceTraveledX += deltaX;
+            currentDragX += deltaX;
         }
 
         if (newY <= 500 && newY >= -1400) {
             map.style.backgroundPositionY = `${newY}px`;
-            distanceTraveledY += deltaY;
+            currentDragY += deltaY;
         }
-    } else {
-        handleHover(e);
     }
 }
 
+// Event listeners for hover - mouse and touch
+map.addEventListener('mousemove', handleHover);
+map.addEventListener('touchmove', handleHover);
 
+function handleHover(e) {
+    if(!isDragging){
+
+    // Get the screen resolution
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
+
+    // Calculate the scale factors for width and height
+    let widthScaleFactor = screenWidth / mapWidth;
+    let heightScaleFactor = screenHeight / mapHeight;
+
+    // Use the maximum of the two scale factors
+    let sizeFactor = Math.max(widthScaleFactor, heightScaleFactor);
+
+    const absPosX = (e.clientX - totalDragX) / sizeFactor;
+    const absPosY = (e.clientY - totalDragY) / sizeFactor;
+
+    //temp
+    if (570.0 <= absPosX && absPosX <= 670.0 && 600.0 <= absPosY && absPosY <= 700.0) {
+        // alert("גמרתי");
+        console.log("Hovering on mon");
+    }
+
+    // updating the coordinates at the bottom of the screen:
+    const coordinatesElement = document.getElementById('coords');
+    const clientX = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
+    const clientY = e.type === 'mousemove' ? e.clientY : e.touches[0].clientY;
+    coordinatesElement.textContent = `(${clientX - totalDragX}, ${clientY - totalDragY}), (${Math.round(absPosX)}, ${Math.round(absPosY)})`;
+    } 
+}
+
+
+// Event listeners for ending drag - mouse and touch
+document.addEventListener('mouseup', handleDragEnd);
+document.addEventListener('touchend', handleDragEnd);
+
+function handleDragEnd() {
+    if (isDragging) {
+        isDragging = false;
+        totalDragX += currentDragX;
+        totalDragY += currentDragY;
+        currentDragX = 0;
+        currentDragY = 0;
+        map.style.cursor = "move";
+    }
+}
+
+// Determining map height and width:
 
 const backgroundImage = getComputedStyle(document.querySelector('.map')).backgroundImage;
 
@@ -75,67 +125,8 @@ const mapImage = new Image();
 // Set the source to the background image URL
 mapImage.src = imageUrl;
 
-// Access dimensions after the image has loaded
-mapImage.onload = function() {
+// Access dimensions after the map has loaded
+mapImage.onload = function () {
     mapWidth = mapImage.width;
     mapHeight = mapImage.height;
 };
-
-
-
-function handleHover(e) {
-    // Handle hover logic if needed
-    // Get the screen resolution
-    let screenWidth = window.innerWidth;
-    let screenHeight = window.innerHeight;
-
-    // Calculate the scale factors for width and height
-    let widthScaleFactor = screenWidth / mapWidth;
-    let heightScaleFactor = screenHeight / mapHeight;
-
-    // Use the minimum of the two scale factors
-    let sizeFactor = Math.max(widthScaleFactor, heightScaleFactor);
-
-    console.log('Hover detected');
-    const absPosX = (e.clientX - shiftedX)/sizeFactor;
-    const absPosY = (e.clientY - shiftedY)/sizeFactor;
-    
-    console.log(`${absPosY}`);
-    console.log(`${sizeFactor}`);
-    if( 570.0 <= absPosX && absPosX <= 670.0 &&  600.0 <= absPosY && absPosY <= 700.0){
-        alert("גמרתי");
-        console.log("Hovering on mon");
-        
-    }
-
-    const clientX = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
-    const clientY = e.type === 'mousemove' ? e.clientY : e.touches[0].clientY;
-    coordinatesElement.textContent = `(${clientX - shiftedX}, ${clientY - shiftedY}), (${absPosX}, ${absPosY}), ${sizeFactor}`;
-
-}
-
-// Event listeners for drag end on mouse and touch
-document.addEventListener('mouseup', handleEnd);
-document.addEventListener('touchend', handleEnd);
-
-function handleEnd() {
-    if (isDragging) {
-        isDragging = false;
-        shiftedX += distanceTraveledX;
-        shiftedY += distanceTraveledY;
-        distanceTraveledX = 0;
-        distanceTraveledY = 0;
-        map.style.cursor = "move";
-    }
-}
-
-// Event listeners for updating mouse coordinates on mouse and touch move
-const coordinatesElement = document.getElementById('coords');
-document.addEventListener('mousemove', updateCoordinates);
-document.addEventListener('touchmove', updateCoordinates);
-
-function updateCoordinates(event) {
-    //const clientX = event.type === 'mousemove' ? event.clientX : event.touches[0].clientX;
-    //const clientY = event.type === 'mousemove' ? event.clientY : event.touches[0].clientY;
-    //coordinatesElement.textContent = `(${clientX - shiftedX}, ${clientY - shiftedY}), ${mapHeight}, ${mapWidth}`;
-}
