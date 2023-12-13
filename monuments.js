b31d238134a35c4b3f0899fba8a0f56a66da81fb
Silveronski <153466946+Silveronski@users.noreply.github.com>
2023-12-13 14:31:39 +0200

    lol

--- monuments.js
+++ monuments.js
@@ -55,7 +55,7 @@ class Monument {
         this.mon.style.display = "block";
         this.mon.style.opacity = 0;
         setTimeout(() => {
-            this.mon.style.transition = "opacity 0.3s ease-in-out";
+            this.mon.style.transition = "opacity 0.4s ease-in-out";
             this.mon.style.opacity = 1;
             this.mon.style.transform = `translate(${left}px, ${top}px) translate(-50%, -50%)`;
         }, 0);
