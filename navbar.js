19df2fac7bac90704eb0f72f3db3f5f198bde66c
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-01-03 09:29:22 +0200

    navabr

--- navbar.js
+++ navbar.js
@@ -1,80 +1,77 @@
-document.addEventListener('DOMContentLoaded',async ()=>{
-    try{
-        const resposne = await fetch('navbar.html');
-        const data = await resposne.text();
-        document.getElementById('navbar-container').innerHTML = data;      
-    } 
-    catch(error){
-        console.error("Error loading the navbar",error);
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const resposne = await fetch("navbar.html");
+    const data = await resposne.text();
+    document.getElementById("navbar-container").innerHTML = data;
+  } catch (error) {
+    console.error("Error loading the navbar", error);
+  }
+
+  const navbarContainer = document.querySelector(".navbar ul");
+  navbarContainer.addEventListener("mouseenter", handleNavbarEnter);
+  navbarContainer.addEventListener("touchstart", handleNavbarEnter);
+
+  function handleNavbarEnter() {
+    handleDragEnd();
+    if (currentCardShowing.length !== 0) {
+      currentCardShowing[0].hideMonumentCard();
     }
-    
-    const navbarContainer = document.querySelector(".navbar ul");
-    navbarContainer.addEventListener("mouseenter", handleNavbarEnter)
-    navbarContainer.addEventListener("touchstart", handleNavbarEnter)
-       
-    function handleNavbarEnter(){
-        handleDragEnd();
-        if (currentCardShowing.length !== 0){
-            currentCardShowing[0].hideMonumentCard();     
-        }
+  }
+
+  const mobileMenuOpener = document.getElementById("mobile-menu");
+  const navbar = document.querySelector("#navbar-container .navbar ul");
+  const mobileMenuCloser = document.querySelector(
+    "#navbar-container .navbar ul .close-navbar"
+  );
+  let isMenuOpen = false;
+
+  mobileMenuOpener.addEventListener("click", openNavbarMenu);
+  mobileMenuCloser.addEventListener("click", closeNavbarMenu);
+
+  function openNavbarMenu() {
+    if (!isMenuTransitioning) {
+      navbar.style.animation = "slideInFromRight 0.5s ease";
+      navbar.style.display = "block";
+      isMenuTransitioning = true;
+      isMenuOpen = true;
+      if (currentCardShowing.length !== 0) {
+        currentCardShowing[0].hideMonumentCard();
+      }
     }
+  }
 
+  function closeNavbarMenu() {
+    navbar.style.animation = "slideInFromLeft 0.5s ease";
+    navbar.addEventListener("animationend", onAnimationEnd, { once: true });
+  }
 
-    const mobileMenuOpener = document.getElementById('mobile-menu');
-    const navbar = document.querySelector("#navbar-container .navbar ul");
-    const mobileMenuCloser = document.querySelector("#navbar-container .navbar ul .close-navbar") 
-    let isMenuOpen = false;
+  function onAnimationEnd() {
+    navbar.style.display = "none";
+    isMenuTransitioning = false;
+    isMenuOpen = false;
+  }
 
-    mobileMenuOpener.addEventListener('click', openNavbarMenu);
-    mobileMenuCloser.addEventListener('click', closeNavbarMenu);
-
-    function openNavbarMenu(){
-        if (!isMenuTransitioning){      
-            navbar.style.animation = "slideInFromRight 0.5s ease"
-            navbar.style.display = "block";
-            isMenuTransitioning = true;
-            isMenuOpen = true;
-            if (currentCardShowing.length !==0){
-                currentCardShowing[0].hideMonumentCard();
-            }                  
-        }     
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+      navbar.style.display = "flex";
+    } else {
+      if (isMenuOpen) {
+        closeNavbarMenu();
+      }
     }
+  });
 
-    function closeNavbarMenu(){
-        navbar.style.animation = "slideInFromLeft 0.5s ease";
-        navbar.addEventListener('animationend', onAnimationEnd, { once: true });
+  const logo = document.getElementById("logo");
+  logo.addEventListener("click", closeNavbarMenuFromLogo);
+
+  function closeNavbarMenuFromLogo(e) {
+    if (window.location.href.includes("index.html")) {
+      if (isMenuOpen) {
+        e.preventDefault();
+        closeNavbarMenu();
+      } else {
+        e.preventDefault();
+      }
     }
-
-    function onAnimationEnd() {
-        navbar.style.display = "none";
-        isMenuTransitioning = false;
-        isMenuOpen = false;
-    }
-
-    window.addEventListener('resize',()=>{
-        if (window.innerWidth >= 1024){
-            navbar.style.display = "flex";
-        }
-        else{
-            if (isMenuOpen){
-                closeNavbarMenu();
-            }
-        }
-    }); 
-
-
-    const logo = document.getElementById('logo');
-    logo.addEventListener('click', closeNavbarMenuFromLogo);
-
-    function closeNavbarMenuFromLogo(e){
-        if (window.location.href.includes('index.html')){
-            if (isMenuOpen){
-                e.preventDefault()
-                closeNavbarMenu();
-            }
-            else{
-                e.preventDefault();  
-            }                 
-        }
-    }
+  }
 });
